a0a63ae4dd01a60c26f5b98ed1748a259a2f9a16
ftam <faroq.al.tam@gmail.com>
2019-04-18 11:59:13 +0100

    coding

--- mse_gradient_desc.py
+++ mse_gradient_desc.py
@@ -1,2 +1,15 @@
 # gradient descent python using torch
 
+from matplotlib import pyplot as plt
+import torch
+
+
+n = 100
+x = torch.ones(n,2)
+x[:, 0].uniform_(-1, 1)
+a = torch.tensor([ 2., 3])
+y = x@a
+
+
+plt.scatter(x[:, 0], y)
+plt.show()
